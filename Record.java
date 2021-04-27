187793cb41b85ac0b5f45ca53ab397abc1e6adfa
ahbyeol123 <abbyinkr@gmail.com>
2021-04-27 17:19:55 +0900

    27

---
+++ Record.java
@@ -0,0 +1,34 @@
+public class Record
+{
+
+	// 학생 1명당 속성
+	String name;									//--이름
+	int[] score = new int[3];//int kor, eng, mat	//--국어, 영어, 수학 점수
+	int tot;										//--총점
+	double avg;										//--평균
+	int rank;										//--석차
+	
+	/*
+		
+	String name;				
+	int[] score = new int[3];			
+	int sum;					
+	double avg;						
+	int rank;					
+
+	
+
+	// setter
+	void setName(String name)
+	{
+		this.name = name;
+	}
+
+	// getter
+	String getName()
+	{
+		return name;
+	}
+	*/
+}
+
